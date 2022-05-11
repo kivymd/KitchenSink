0cfeae83dcc62961c1457513c37a97781c94785c
macbookair <kivydevelopment@gmail.com>
2022-05-11 06:16:06 +0300

    Fix `RoundCard` impot

--- View/FieldScreen/field_screen.py
+++ View/FieldScreen/field_screen.py
@@ -3,7 +3,8 @@ from kivymd.uix.screen import MDScreen
 
 from View.FieldScreen import OneScreen, TwoScreen, Dots  # NOQA
 from View.FieldScreen.components.thirdscreen.third_screen import ThirdScreen  # NOQA
-from View.common import RoundCard  # NOQA
+from View.common.round_card import RoundCard  # NOQA
+
 from View.slide_animatior import SlideAnimatior
 
 
