eb70d328f584d19a14c5348522bdc715f7343cbb
macbookair <kivydevelopment@gmail.com>
2022-05-11 08:31:59 +0300

    Add `SliverScreen` package

---
+++ View/SliverScreen/sliver_screen.py
@@ -0,0 +1,16 @@
+import os
+
+from kivymd.uix.screen import MDScreen
+
+from View.SliverScreen.components.slivercard.sliver_card import SliverCard
+
+
+class SliverScreenView(MDScreen):
+    def on_enter(self):
+        if not self.ids.content.children:
+            for x in range(10):
+                self.ids.content.add_widget(
+                    SliverCard(
+                        image=f"{os.environ['KITCHEN_SINK_ASSETS']}/images/sliver_screen/avatar.png"
+                    )
+                )
