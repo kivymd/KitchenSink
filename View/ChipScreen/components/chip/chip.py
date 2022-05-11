103577ce09ae5fb23b7925fced163798feb403aa
macbookair <kivydevelopment@gmail.com>
2022-05-11 08:04:57 +0300

    Edit with `Black` utility

--- View/ChipScreen/components/chip/chip.py
+++ View/ChipScreen/components/chip/chip.py
@@ -9,10 +9,12 @@ class CustomChip(MDChip):
     md_bg_color = (0, 0, 0, 0)
     line_color = get_color_from_hex("#ecaa98")
     text_color = get_color_from_hex("#442c2e")
-    radius = [8,]
+    radius = [
+        8,
+    ]
     _no_ripple_effect = True
 
     def set_chip_text_color(self, instance_chip, active_value: int):
-        Animation(
-            color=(0, 0, 0, 1) if active_value else (0, 0, 0, 0.5), d=0.2
-        ).start(self.ids.label)
+        Animation(color=(0, 0, 0, 1) if active_value else (0, 0, 0, 0.5), d=0.2).start(
+            self.ids.label
+        )
