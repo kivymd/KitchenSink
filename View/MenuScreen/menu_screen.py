8471e32b843e25c4fdf4849907df6f893aea37fd
macbookair <kivydevelopment@gmail.com>
2022-05-11 08:05:21 +0300

    Edit with `Black` utility

--- View/MenuScreen/menu_screen.py
+++ View/MenuScreen/menu_screen.py
@@ -8,13 +8,15 @@ from View.MenuScreen.componemts import MenuCard  # NOQA
 class MenuScreenView(MDScreen):
     def on_enter(self, *args) -> None:
         if not self.ids.menu_list.data:
-            for name_card in ["Field", "Card", "Button", "Chip", "List"]:
+            for name_card in ["Field", "Card", "Button", "Chip", "List", "Sliver"]:
                 self.ids.menu_list.data.append(
                     {
                         "viewclass": "MenuCard",
                         "title": name_card,
                         "elevation": 12,
-                        "on_release": lambda x=name_card.lower(): self.manager.switch_screen(x),
+                        "on_release": lambda x=name_card.lower(): self.manager.switch_screen(
+                            x
+                        ),
                         "source": (
                             f"{os.environ['KITCHEN_SINK_ASSETS']}/"
                             f"images/menu_screen/{name_card.lower()}.png"
