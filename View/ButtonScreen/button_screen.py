bddc4da67adf9f659a9d58bb0039b2fdd9e1869e
macbookair <kivydevelopment@gmail.com>
2022-05-13 17:06:53 +0300

    The screen has been changed to use multiple screens
    The screen has been changed to use multiple screens with the `MDCarousel` widget.

--- View/ButtonScreen/button_screen.py
+++ View/ButtonScreen/button_screen.py
@@ -1,5 +1,11 @@
+from kivymd.theming import ThemableBehavior
 from kivymd.uix.screen import MDScreen
 
+from View.ButtonScreen.components import ButtonScreenOneScreen, ButtonScreenTwoScreen  # NOQA
+from View.common.dots import Dots  # NOQA
+from View.common.round_card import RoundCard  # NOQA
+from View.slide_animatior import SlideAnimatior
 
-class ButtonScreenView(MDScreen):
+
+class ButtonScreenView(ThemableBehavior, MDScreen, SlideAnimatior):
     pass
