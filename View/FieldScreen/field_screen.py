21cc73b509aa1ea4348e8cc36b1b4b21ddcb9e63
macbookair <kivydevelopment@gmail.com>
2022-05-13 17:17:52 +0300

    Fix imports

--- View/FieldScreen/field_screen.py
+++ View/FieldScreen/field_screen.py
@@ -1,17 +1,12 @@
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.screen import MDScreen
 
-from View.FieldScreen import OneScreen, TwoScreen, Dots  # NOQA
-from View.FieldScreen.components.thirdscreen.third_screen import ThirdScreen  # NOQA
+from View.FieldScreen.components import OneScreen, TwoScreen, ThirdScreen  # NOQA
+from View.common.dots import Dots  # NOQA
 from View.common.round_card import RoundCard  # NOQA
 
 from View.slide_animatior import SlideAnimatior
 
 
-class FieldScreenView(
-    ThemableBehavior,
-    MDScreen,
-    SlideAnimatior,
-):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+class FieldScreenView(ThemableBehavior, MDScreen, SlideAnimatior):
+    pass
