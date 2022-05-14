862cb93dc7554403a61627ed4208c5026a55b5e4
macbookair <kivydevelopment@gmail.com>
2022-05-14 18:36:00 +0300

    Fixed infinite list creation when opening the screen
    Added data validation for the RecycleView item list.
    Otherwise, each time the screen was opened, new items were added to the list.

--- View/ImageListScreen/imagelist_screen.py
+++ View/ImageListScreen/imagelist_screen.py
@@ -8,20 +8,21 @@ from View.ImageListScreen.components import ImageListScreenTile, ImageListScreen
 class ImageListScreenView(MDScreen):
     def on_enter(self, *args):
         for i in range(6):
-            self.ids.grid_list.data.append(
-                {
-                    "viewclass": "ImageListScreenTile",
-                    "icon": "book-open-outline",
-                    "source":
-                        f"{os.environ['KITCHEN_SINK_ASSETS']}/"
-                        f"images/imagelist_screen/{i+1}.jpg",
-                    "text": {
-                        1: "Classic",
-                        2: "Leisure",
-                        3: "Children's books",
-                        4: "E-books",
-                        5: "Special literature",
-                        6: "Books for study",
-                    }[i+1],
-                }
-            )
+            if not self.ids.grid_list.data:
+                self.ids.grid_list.data.append(
+                    {
+                        "viewclass": "ImageListScreenTile",
+                        "icon": "book-open-outline",
+                        "source":
+                            f"{os.environ['KITCHEN_SINK_ASSETS']}/"
+                            f"images/imagelist_screen/{i+1}.jpg",
+                        "text": {
+                            1: "Classic",
+                            2: "Leisure",
+                            3: "Children's books",
+                            4: "E-books",
+                            5: "Special literature",
+                            6: "Books for study",
+                        }[i+1],
+                    }
+                )
