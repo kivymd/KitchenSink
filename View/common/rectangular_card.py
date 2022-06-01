c0fa763d5458bf7901fbcdb817bfa265769affae
macbookair <kivydevelopment@gmail.com>
2022-06-01 21:53:28 +0300

    Remove `rectangular_card.py` and `round_card.py` modules

--- View/common/rectangular_card.py
+++
@@ -1,6 +0,0 @@
-from kivymd.uix.behaviors import FakeRectangularElevationBehavior
-from kivymd.uix.card import MDCard
-
-
-class RectangularCard(MDCard, FakeRectangularElevationBehavior):
-    pass
--- View/common/round_card.py
+++
@@ -1,6 +0,0 @@
-from kivymd.uix.behaviors import FakeCircularElevationBehavior
-from kivymd.uix.card import MDCard
-
-
-class RoundCard(MDCard, FakeCircularElevationBehavior):
-    pass
