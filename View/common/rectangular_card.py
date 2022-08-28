dca4984b294c2d78fa4b8c60defdb986b8fd61e4
macbookair <kivydevelopment@gmail.com>
2022-08-28 08:34:31 +0300

    Remove parent class

--- View/common/rectangular_card.py
+++ View/common/rectangular_card.py
@@ -1,6 +1,5 @@
-from kivymd.uix.behaviors import FakeRectangularElevationBehavior
 from kivymd.uix.card import MDCard
 
 
-class RectangularCard(MDCard, FakeRectangularElevationBehavior):
+class RectangularCard(MDCard):
     pass
