e00f41e0fe65487f7dfce2612a3e366c63f1e03c
Yuri Ivanov <kivydevelopment@gmail.com>
2022-09-03 22:39:36 +0300

    Removed the parent class `FakeCircularElevationBehavior` from the `RoundCard` class

--- View/common/round_card.py
+++ View/common/round_card.py
@@ -1,6 +1,5 @@
-from kivymd.uix.behaviors import FakeCircularElevationBehavior
 from kivymd.uix.card import MDCard
 
 
-class RoundCard(MDCard, FakeCircularElevationBehavior):
+class RoundCard(MDCard):
     pass
